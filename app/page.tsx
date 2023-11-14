76a910ca49731b75ce116e66fbfc2209d4cbcf96
CurtMicro <curtismaple1@hotmail.com>
2023-11-14 10:17:29 -0600

    feature P-3 change second model

--- app/page.tsx
+++ app/page.tsx
@@ -7,25 +7,43 @@ import { Environment, OrbitControls } from "@react-three/drei";
 
 export default function Home() {
 
-  const Model = () => {
-    const gltf = useLoader(GLTFLoader, "/models/future_car/scene.gltf");
-
+  const CarModel = () => {
+    const car = useLoader(GLTFLoader, "/models/future_car/scene.gltf");
     return (
       <>
+        <primitive dispose={null} object={car.scene} scale={2} />
+      </>
+    );
+  }
 
-        <primitive dispose={null} object={gltf.scene} scale={2} />
+  const LamboModel = () => {
+    const lambo = useLoader(GLTFLoader, "/models/lambo/scene.gltf");
+    return (
+      <>
+        <primitive dispose={null} object={lambo.scene} scale={2} />
       </>
     );
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex w-screen  min-h-screen items-center justify-evenly flex-wrap">
       <ThreeDContainer>
-        <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
+        <Canvas shadows dpr={[1, 2]} camera={{ position: [10, 10, 5], fov: 80 }}>
           <ambientLight intensity={0.7} />
-          <spotLight intensity={0.5} angle={0.1} penumbra={2} position={[10, 15, 10]} castShadow />
+          <spotLight intensity={0.8} angle={0.1} penumbra={6} position={[10, 15, 10]} castShadow />
           <Suspense fallback={null}>
-            <Model />
+            <CarModel />
+            <Environment preset="city" />
+          </Suspense>
+          <OrbitControls autoRotate />
+        </Canvas>
+      </ThreeDContainer>
+      <ThreeDContainer>
+        <Canvas shadows dpr={[1, 2]} camera={{ position: [5, 5, 2], fov: 80 }}>
+          <ambientLight intensity={0.7} />
+          <spotLight intensity={0.8} angle={50} penumbra={6} position={[10, 15, 10]} castShadow />
+          <Suspense fallback={null}>
+            <LamboModel />
             <Environment preset="city" />
           </Suspense>
           <OrbitControls autoRotate />
@@ -37,7 +55,8 @@ export default function Home() {
 
 const ThreeDContainer = styled.div`
   canvas {
-    height:600px !important;
-    width:600px !important;
+    border: 1px solid red;
+    height:500px !important;
+    width:500px !important;
   }
 `;
